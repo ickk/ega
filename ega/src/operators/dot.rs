5bb05d44817f5518b65eb1560332779c8fac878a
ickk <git@ickk.io>
2023-09-07 02:31:56 +1000

    wip test `Dot`

--- ega/src/operators/dot.rs
+++ ega/src/operators/dot.rs
@@ -651,108 +651,208 @@ fn pseudoscalar_dot_pseudoscalar(_: &Pseudoscalar, _: &Pseudoscalar) -> Empty {
   Empty
 }
 
+#[rustfmt::skip]
 #[cfg(any(test, doctest))]
 mod tests {
   use super::*;
 
-  /// [
-  ///       e0,    e1,    e2,    e3,
-  ///   scalar,   e23,   e31,   e12,
-  ///      e01,   e02,   e03, e0123,
-  ///     e123,  e032,  e013,  e021,
-  /// ]
-  #[rustfmt::skip]
-  #[test]
-  fn multivector_dot_multivector() {
-    {
-      // [bivector.net evaluator](https://bivector.net/tools.html#seven)
-      // (
-      //      2e0   +  3e1   +  5e2   +  7e3
-      //   + 11     + 13e23  + 17e31  + 19e12
-      //   + 23e01  + 29e02  + 31e03  + 37e0123
-      //   + 41e123 + 43e032 + 47e013 + 53e021
-      // )
-      // |
-      // (
-      //      59e0   +  61e1   + 67e2    + 71e3
-      //   +  73     +  79e23  + 83e31   + 89e12
-      //   +  97e01  + 101e02  + 103e03  + 107e0123
-      //   + 109e123 + 113e032 + 127e013 + 131e021
-      // )
-      // =
-      // [
-      //   23311e0,   −3564e1,   −4676e2,  −4116e3,
-      //   −6780,      4646e23,   5446e31,  6040e12,
-      //   −1548e01,  −1880e02,  −1732e03,  3878e0123,
-      //    4192e123,  2446e032,  2884e013, 3432e021,
-      // ];
-      let a = Multivector {
-          e0:  2.,   e1:  3.,   e2:  5.,    e3:  7.,
-           s: 11.,  e23: 13.,  e31: 17.,   e12: 19.,
-         e01: 23.,  e02: 29.,  e03: 31., e0123: 37.,
-        e123: 41., e032: 43., e013: 47.,  e021: 53.,
-      };
-      let b = Multivector {
-          e0:  59.,   e1:  61.,   e2:  67.,    e3:  71.,
-           s:  73.,  e23:  79.,  e31:  83.,   e12:  89.,
-         e01:  97.,  e02: 101.,  e03: 103., e0123: 107.,
-        e123: 109., e032: 113., e013: 127.,  e021: 131.,
-      };
-      let result = a.dot(&b);
+  use super::*;
+  use crate::operators::Zero;
 
+  // we use prime numbers to reduce the chances of a falsely passing test.
+  const MULTIVECTOR_A: Multivector = Multivector {
+      e0:    2.,   e1:    3.,   e2:    5.,    e3:    7.,
+       s:   11.,  e23:   13.,  e31:   17.,   e12:   19.,
+     e01:   23.,  e02:   29.,  e03:   31., e0123:   37.,
+    e123:   41., e032:   43., e013:   47.,  e021:   53.,
+  };
+  const MULTIVECTOR_B: Multivector = Multivector {
+      e0:   59.,   e1:   61.,   e2:   67.,    e3:   71.,
+       s:   73.,  e23:   79.,  e31:   83.,   e12:   89.,
+     e01:   97.,  e02:  101.,  e03:  103., e0123:  107.,
+    e123:  109., e032:  113., e013:  127.,  e021:  131.,
+  };
+  const MULTIVECTOR_C: Multivector = Multivector {
+      e0:  -59.,   e1:   61.,   e2:  -67.,    e3:   71.,
+       s:  -73.,  e23:   79.,  e31:  -83.,   e12:   89.,
+     e01:  -97.,  e02:  101.,  e03: -103., e0123:  107.,
+    e123: -109., e032:  113., e013: -127.,  e021:  131.,
+  };
+  const MULTIVECTOR_D: Multivector = Multivector {
+      e0:  -59.,   e1:  -61.,   e2:  -67.,    e3:  -71.,
+       s:  -73.,  e23:  -79.,  e31:  -83.,   e12:  -89.,
+     e01:  -97.,  e02: -101.,  e03: -103., e0123: -107.,
+    e123: -109., e032: -113., e013: -127.,  e021: -131.,
+  };
+  const SCALAR_A: Scalar = Scalar { s:  137. };
+  const SCALAR_B: Scalar = Scalar { s:  139. };
+  const SCALAR_C: Scalar = Scalar { s: -149. };
+  const VECTOR_A: Vector = Vector {
+    e0:  151., e1:  157., e2:  163., e3:  167.
+  };
+  const VECTOR_B: Vector = Vector {
+    e0:  173., e1:  179., e2:  181., e3:  191.
+  };
+  const VECTOR_C: Vector = Vector {
+    e0:  -193., e1: -197., e2:  -199., e3: -211.
+  };
+  const BIVECTOR_A: Bivector = Bivector {
+    e23:  223., e31:  227., e12:  229.,
+    e01:  233., e02:  239., e03:  241.,
+  };
+  const BIVECTOR_B: Bivector = Bivector {
+    e23:  251., e31:  257., e12:  263.,
+    e01:  269., e02:  271., e03:  277.,
+  };
+  const BIVECTOR_C: Bivector = Bivector {
+    e23: -281., e31: -283., e12: -293.,
+    e01: -307., e02: -311., e03: -313.,
+  };
+  const TRIVECTOR_A: Trivector = Trivector {
+    e123:  317., e032:  331., e013:  337., e021:  347.
+  };
+  const _TRIVECTOR_B: Trivector = Trivector {
+    e123:  349., e032:  353., e013:  359., e021:  367.
+  };
+  const TRIVECTOR_C: Trivector = Trivector {
+    e123: -373., e032: -379., e013: -383., e021: -389.
+  };
+  const PSEUDOSCALAR_A: Pseudoscalar = Pseudoscalar { e0123:  397. };
+  const _PSEUDOSCALAR_B: Pseudoscalar = Pseudoscalar { e0123:  401. };
+  const PSEUDOSCALAR_C: Pseudoscalar = Pseudoscalar { e0123: -409. };
+
+  mod multivector {
+    use super::*;
+    #[test]
+    fn dot_multivector_1() {
+      let result = MULTIVECTOR_A.dot(&MULTIVECTOR_B);
       let expected = Multivector {
           e0: 23311.,   e1: -3564.,   e2: -4676.,    e3: -4116.,
-           s: -6780.,  e23:  4646.,  e31:  5446.,   e12: 6040.,
-         e01: -1548.,  e02: -1880.,  e03: -1732., e0123: 3878.,
-        e123:  4192., e032:  2446., e013:  2884.,  e021: 3432.,
+           s: -6780.,  e23:  4646.,  e31:  5446.,   e12:  6040.,
+         e01: -1548.,  e02: -1880.,  e03: -1732., e0123:  3878.,
+        e123:  4192., e032:  2446., e013:  2884.,  e021:  3432.,
       };
-
       assert_eq!(dbg!(result), dbg!(expected));
     }
-
-    {
-      // [bivector.net evaluator](https://bivector.net/tools.html#seven)
-      // (
-      //      2e0   +  3e1   +  5e2   +  7e3
-      //   + 11     + 13e23  + 17e31  + 19e12
-      //   + 23e01  + 29e02  + 31e03  + 37e0123
-      //   + 41e123 + 43e032 + 47e013 + 53e021
-      // )
-      // |
-      // (
-      //   -  59e0   +  61e1   - 67e2    + 71e3
-      //   -  73     +  79e23  - 83e31   + 89e12
-      //   -  97e01  + 101e02  - 103e03  + 107e0123
-      //   - 109e123 + 113e032 - 127e013 + 131e021
-      // )
-      // =
-      // [
-      //   15805e0,   -4876e1,    3632e2,    1244e3,
-      //    2704,      2094e23,  -5446e31,   1740e12,
-      //   -1716e01,     28e02, -13524e03,  -1524e0123,
-      //   -4192e123, -3832e032, -1814e013, -4306e021,
-      // ];
-      let a = Multivector {
-          e0:  2.,   e1:  3.,   e2:  5.,    e3:  7.,
-           s: 11.,  e23: 13.,  e31: 17.,   e12: 19.,
-         e01: 23.,  e02: 29.,  e03: 31., e0123: 37.,
-        e123: 41., e032: 43., e013: 47.,  e021: 53.,
-      };
-      let b = Multivector {
-          e0:  -59.,   e1:  61.,   e2:  -67.,    e3:  71.,
-           s:  -73.,  e23:  79.,  e31:  -83.,   e12:  89.,
-         e01:  -97.,  e02: 101.,  e03: -103., e0123: 107.,
-        e123: -109., e032: 113., e013: -127.,  e021: 131.,
-      };
-      let result = a.dot(&b);
+    #[test]
+    fn dot_multivector_2() {
+      let result = MULTIVECTOR_A.dot(&MULTIVECTOR_C);
       let expected = Multivector {
           e0: 15805.,   e1: -4876.,   e2:   3632.,    e3:  1244.,
            s:  2704.,  e23:  2094.,  e31:  -5446.,   e12:  1740.,
          e01: -1716.,  e02:    28.,  e03: -13524., e0123: -1524.,
         e123: -4192., e032: -3832., e013:  -1814.,  e021: -4306.,
       };
-
       assert_eq!(dbg!(result), dbg!(expected));
     }
+    #[test]
+    fn dot_multivector_3() {
+      let result = MULTIVECTOR_A.dot(&MULTIVECTOR_D);
+      let expected = Multivector {
+          e0: -23311.,   e1:  3564.,   e2:  4676.,    e3:  4116.,
+           s:   6780.,  e23: -4646.,  e31: -5446.,   e12: -6040.,
+         e01:   1548.,  e02:  1880.,  e03:  1732., e0123: -3878.,
+        e123:  -4192., e032: -2446., e013: -2884.,  e021: -3432.,
+      };
+      assert_eq!(dbg!(result), dbg!(expected));
+    }
+    // #[test]
+    // fn dot_scalar_1() {
+    //   let result = MULTIVECTOR_A.dot(&SCALAR_A);
+    //   let expected = Multivector {
+    //       e0:   274.,   e1:   411.,   e2:   685.,    e3:   959.,
+    //        s:  1507.,  e23:  1781.,  e31:  2329.,   e12:  2603.,
+    //      e01:  3151.,  e02:  3973.,  e03:  4247., e0123:  5069.,
+    //     e123:  5617., e032:  5891., e013:  6439.,  e021:  7261.,
+    //   };
+    //   assert_eq!(dbg!(result), dbg!(expected));
+    // }
+    // #[test]
+    // fn dot_scalar_2() {
+    //   let result = MULTIVECTOR_A.dot(&SCALAR_C);
+    //   let expected = Multivector {
+    //       e0:  -298.,   e1:  -447.,   e2:  -745.,    e3: -1043.,
+    //        s: -1639.,  e23: -1937.,  e31: -2533.,   e12: -2831.,
+    //      e01: -3427.,  e02: -4321.,  e03: -4619., e0123: -5513.,
+    //     e123: -6109., e032: -6407., e013: -7003.,  e021: -7897.,
+    //   };
+    //   assert_eq!(dbg!(result), dbg!(expected));
+    // }
+    // #[test]
+    // fn dot_vector_1() {
+    //   let result = MULTIVECTOR_A.dot(&VECTOR_A);
+    //   let expected = Multivector {
+    //       e0:  1661.,   e1:  1727.,   e2:  1793.,    e3:   1837.,
+    //        s:     0.,  e23:  -306.,  e31:   598.,   e12:   -296.,
+    //      e01:  -139.,  e02:  -429.,  e03:  -723., e0123: -29454.,
+    //     e123:  7985., e032: -1753., e013: -3593.,  e021:  -2065.,
+    //   };
+    //   assert_eq!(dbg!(result), dbg!(expected));
+    // }
+    // #[test]
+    // fn dot_vector_2() {
+    //   let result = MULTIVECTOR_A.dot(&VECTOR_C);
+    //   let expected = Multivector {
+    //       e0: -2123.,   e1: -2167.,   e2: -2189.,    e3:  -2321.,
+    //        s:     0.,  e23:   338.,  e31:  -746.,   e12:    388.,
+    //      e01:   185.,  e02:   567.,  e03:   929., e0123:  36920.,
+    //     e123: -9953., e032:  2459., e013:  4535.,  e021:   2531.,
+    //   };
+    //   assert_eq!(dbg!(result), dbg!(expected));
+    // }
+    // #[test]
+    // fn dot_bivector_1() {
+    //   let result = MULTIVECTOR_A.dot(&BIVECTOR_A);
+    //   let expected = Multivector {
+    //        s:     0.,  e23:  2453.,  e31:  2497.,   e12:   2519.,
+    //      e01:  2563.,  e02:  2629.,  e03:  2651., e0123:  30482.,
+    //     e123:  3407., e032:  -914., e013:   454.,  e021:   -906.,
+    //     ..Multivector::zero()
+    //   };
+    //   assert_eq!(dbg!(result), dbg!(expected));
+    // }
+    // #[test]
+    // fn dot_bivector_2() {
+    //   let result = MULTIVECTOR_A.dot(&BIVECTOR_C);
+    //   let expected = Multivector {
+    //        s:     0.,  e23: -3091.,  e31: -3113.,   e12:  -3223.,
+    //      e01: -3377.,  e02: -3421.,  e03: -3443., e0123: -38978.,
+    //     e123: -4309., e032:  1174., e013:  -644.,  e021:   1188.,
+    //     ..Multivector::zero()
+    //   };
+    //   assert_eq!(dbg!(result), dbg!(expected));
+    // }
+    // #[test]
+    // fn dot_trivector_1() {
+    //   let result = MULTIVECTOR_A.dot(&TRIVECTOR_A);
+    //   let expected = Multivector {
+    //     e123: 3487., e032: 3641., e013: 3707., e021: 3817.,
+    //     e0123: 5741.,
+    //     ..Multivector::zero()
+    //   };
+    //   assert_eq!(dbg!(result), dbg!(expected));
+    // }
+    // #[test]
+    // fn dot_trivector_2() {
+    //   let result = MULTIVECTOR_A.dot(&TRIVECTOR_C);
+    //   let expected = Multivector {
+    //     e123: -4103., e032: -4169., e013: -4213., e021: -4279.,
+    //     e0123: -6521.,
+    //     ..Multivector::zero()
+    //   };
+    //   assert_eq!(dbg!(result), dbg!(expected));
+    // }
+    // #[test]
+    // fn dot_pseudoscalar_1() {
+    //   let result = MULTIVECTOR_A.dot(&PSEUDOSCALAR_A);
+    //   let expected = Pseudoscalar { e0123: 4367. };
+    //   assert_eq!(dbg!(result), dbg!(expected));
+    // }
+    // #[test]
+    // fn dot_pseudoscalar_2() {
+    //   let result = MULTIVECTOR_A.dot(&PSEUDOSCALAR_C);
+    //   let expected = Pseudoscalar { e0123: -4499. };
+    //   assert_eq!(dbg!(result), dbg!(expected));
+    // }
   }
 }

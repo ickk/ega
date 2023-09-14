f16f62e5c2ae0a10a71ede0729fc1250bba73d8b
ickk <git@ickk.io>
2023-09-14 11:06:00 +1000

    add a couple of tests for `Inverse`

--- ega/src/operators/inverse.rs
+++ ega/src/operators/inverse.rs
@@ -40,7 +40,7 @@ mod tests {
   use ::approx::assert_relative_eq;
 
   #[test]
-  fn inverse_scalar() {
+  fn inverse_scalar_a() {
     let inverse = SCALAR_A.inverse();
     let product = SCALAR_A * inverse;
 
@@ -49,7 +49,25 @@ mod tests {
   }
 
   #[test]
-  fn inverse_vector() {
+  fn inverse_scalar_b() {
+    let inverse = SCALAR_B.inverse();
+    let product = SCALAR_B * inverse;
+
+    let expected = Scalar::UNIT;
+    assert_relative_eq!(dbg!(expected), dbg!(product));
+  }
+
+  #[test]
+  fn inverse_scalar_c() {
+    let inverse = SCALAR_C.inverse();
+    let product = SCALAR_C * inverse;
+
+    let expected = Scalar::UNIT;
+    assert_relative_eq!(dbg!(expected), dbg!(product));
+  }
+
+  #[test]
+  fn inverse_vector_a() {
     let inverse = VECTOR_A.inverse();
     let product = VECTOR_A * inverse;
 
@@ -58,9 +76,45 @@ mod tests {
   }
 
   #[test]
-  fn inverse_trivector_1() {
+  fn inverse_vector_b() {
+    let inverse = VECTOR_B.inverse();
+    let product = VECTOR_B * inverse;
+
+    let expected = Multivector::from(Scalar::UNIT);
+    assert_relative_eq!(dbg!(expected), dbg!(product));
+  }
+
+  #[test]
+  fn inverse_vector_c() {
+    let inverse = VECTOR_C.inverse();
+    let product = VECTOR_C * inverse;
+
+    let expected = Multivector::from(Scalar::UNIT);
+    assert_relative_eq!(dbg!(expected), dbg!(product));
+  }
+
+  #[test]
+  fn inverse_trivector_a() {
     let inverse = TRIVECTOR_A.inverse();
     let product = TRIVECTOR_A * inverse;
+
+    let expected = Multivector::from(Scalar::UNIT);
+    assert_relative_eq!(dbg!(expected), dbg!(product));
+  }
+
+  #[test]
+  fn inverse_trivector_b() {
+    let inverse = TRIVECTOR_B.inverse();
+    let product = TRIVECTOR_B * inverse;
+
+    let expected = Multivector::from(Scalar::UNIT);
+    assert_relative_eq!(dbg!(expected), dbg!(product));
+  }
+
+  #[test]
+  fn inverse_trivector_c() {
+    let inverse = TRIVECTOR_C.inverse();
+    let product = TRIVECTOR_C * inverse;
 
     let expected = Multivector::from(Scalar::UNIT);
     assert_relative_eq!(dbg!(expected), dbg!(product));
